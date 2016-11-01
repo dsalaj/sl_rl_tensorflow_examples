d9d05802e6fb6ae3319057fa0358e8abbde4d578
Sinisa <sinisa.stekovic@student.tugraz.at>
2016-11-01 11:33:13 +0100

    Ex3 solved, problem specific

--- ex3_main.py
+++ ex3_main.py
@@ -37,8 +37,8 @@ from gradient_descent_finite_difference import GDFDSolver
 from genetic import GeneticSolver
 
 n_trial_per_call = 10  # Number of trials to estimate the expected accumulated reward at every optimization steps
-render = False
-# render = True  # Run a video of the optimization, set to False TO SAVE COMPUTATION TIME
+# render = False
+render = True  # Run a video of the optimization, set to False TO SAVE COMPUTATION TIME
 bound = [-3, 3]  # Bound of the policy parameters [-3,3] is a reasonable choice, no need to change it
 
 # Load the environment
@@ -81,8 +81,11 @@ def policy(parameter, state):
     :return:
     '''
     #print state
-    #W1 = parameter.reshape((-1,parameter.shape[0]/3))
-    res = int(np.dot(parameter,state) > 0)
+    W1 = parameter[:6].reshape((3,2))
+    W2 = parameter[6:9]
+    a = np.dot(W1,state)
+    y = np.tanh(a)
+    res = int(np.dot(W2,y) > 0)
     if res == 1 : res = 2
 
     return res
@@ -98,57 +101,62 @@ def cost_function(parameter):
     :return:
     '''
     list_of_accumulated_rewards = []
-
+    succ_counter = 0
     for ep in range(n_trial_per_call):
         observation = env.reset()
         acc_reward = 0
         last_action = 0
         min_x = observation[0]
         max_x = observation[0]
+        steps_reward = 0
         for t in range(200):  # The number of time steps in this game is maximum 200
             if render: env.render()
-            #observation = np.concatenate((observation, [last_action - 1]))
-            #print observation
             action = policy(parameter, observation)  # Choose an action according to the policy
-            #print action
-            last_action = action
-            #print observation
-            observation, reward, done, info = env.step(action)  # Move the CartPole
+            observation, reward, done, info = env.step(action)  # Move the Mountain car
             if(observation[0] <= min_x):
                 min_x = observation[0]
             else:
-                max_x = observation[0]
-            #acc_reward += reward  # Accumulate the reward
+                max_x = observation[0]        #print acc_reward
+            steps_reward += reward  # Accumulate the reward
             if done:
-                # If the pole falls, we loose.
+                # If the car reaches flag, we win.
                 break
-        acc_reward = np.abs(min_x-max_x)
+        if steps_reward > -200:
+            # give bonus reward for completion
+            acc_reward = 2
+            # count success for succession rate
+            succ_counter += 1
+        # We measure the score by the distance covered in
+        # simulation
+        acc_reward += np.abs(min_x-max_x)
+        #print acc_reward
         list_of_accumulated_rewards.append(acc_reward)
 
     fitness = sum(list_of_accumulated_rewards)
     fitness /= float(len(list_of_accumulated_rewards))
-    print fitness
-    opt_points = np.max([1, fitness])
-    cost = (opt_points - fitness)**2
 
+    # Cost function is set to 0 to stop the algorithm
+    # if simulation completes the task for n_trial_per_call
+    cost = 1/(fitness)**2
+    if(succ_counter == n_trial_per_call):
+        cost = 0
     return cost
 
 
-solver = AnnealSolver(noisy_step=1.5, temp_decay=.99, n_iteration=100, stop_criterion=0)  # Load the solver object
-#s0 = np.asarray([-2.58080242, -2.47749191])
-s0 = np.zeros(n_state)
-res = solver.solve(s0, cost_function, bound)
+# solver = AnnealSolver(noisy_step=1.5, temp_decay=.99, n_iteration=100, stop_criterion=0)  # Load the solver object
+# s0 = rd.rand((n_state+1)*3 + 3)
+# res = solver.solve(s0, cost_function, bound)
 
 # solver = GDFDSolver(learning_rate=1, exploration_step=1, step_decay= .9, n_random_step=9, n_iteration=100)
 #     #GDFDSolver(learning_rate=.07, exploration_step=.8, step_decay=.97, n_random_step=9, n_iteration=100, stop_criterion=50)
-# s0 = np.zeros(n_state)
-# #s0 = np.asarray([-3., 3.])
+# #s0 = np.asarray([    3.,             3.,          2135.28518004,   -19.66521967])
+# s0 = np.zeros(n_state*3)
 # res = solver.solve(s0, cost_function, bound)
 
-# n_pop = 20
-# solver = GeneticSolver(selection_temperature=1, mutation_rate=.13, crossover_rate=.13, n_iteration=50, stop_criterion=0)
-# pop0 = [rd.rand(5) for k in range(n_pop)]
-# res = solver.solve(pop0, cost_function, bound)
+n_pop = 20
+solver = GeneticSolver(selection_temperature=1, mutation_rate=.13, crossover_rate=.13, n_iteration=50, stop_criterion=0)
+pop0 = [rd.rand((n_state)*3 + 3) for k in range(n_pop)]
+res = solver.solve(pop0, cost_function, bound)
 
 n_trials = res['n_function_call'] * n_trial_per_call
 print('\t Final cost {:.3g} \t in {} trials.'.format(res['f_list'][-1], n_trials))
@@ -159,3 +167,18 @@ ax.plot(res['f_list'], lw=2)
 ax.set_ylabel('Cost functions')
 ax.set_xlabel('Optimization iterations')
 plt.show()
+
+
+# s0 = np.asarray([
+# -0.845226402928 ,
+# 2.40399032615 ,
+# 0.322772761635 ,
+# 0.643287316438 ,
+# 0.605076847906 ,
+# 0.0482834095796 ,
+# 0.482324106981 ,
+# 0.668424652057 ,
+# 0.279595699179 ,
+# ]
+#
+# )
