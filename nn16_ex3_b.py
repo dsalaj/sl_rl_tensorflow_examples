4a73c96a2961675d11bce6b635494910aeb223f8
Darjan Salaj <salaj@gmx.at>
2016-12-05 21:02:42 +0100

    fixed b)

--- nn16_ex3_b.py
+++ nn16_ex3_b.py
@@ -32,22 +32,23 @@ mr_plot_points = []
 eta_plot_points = []
 min_float = 0.00000000000001
 # for eta in [10**i for i in range(-10, 10)]:
-eta = 1e-6
+eta = 0.008
 # eta_plot_points.append(eta)
 w = np.random.random((X.shape[1], 1))
-num_epochs = 5000
+num_epochs = 1000
 for i in range(1, 1+num_epochs):
-    if i % 100 == 0:
-        print i
     for [x, c] in zip(X, C):
         y = sig(np.dot(x, w))
-        a = np.dot(c, np.log(y.clip(min=min_float)))
-        ce = -(a + np.dot((np.ones_like(c) - c), np.log((np.ones_like(y) - y).clip(min=min_float))))
+        # a = np.dot(c, np.log(y.clip(min=min_float)))
+        # ce = -(a + np.dot((np.ones_like(c) - c), np.log((np.ones_like(y) - y).clip(min=min_float))))
         # if np.all(x == X[-1]):
         #     print "for eta=", eta, "error =", ce
 
-        E_ce = np.dot(np.transpose(y - C), X[:])
-        w = w - eta * np.transpose(E_ce)
+        # E_ce = np.dot(np.transpose(y - c), x)
+        x_ = np.reshape(x, (x.shape[0], 1))
+        E_ce = x_.dot(y-c)
+        E_ce = np.reshape(E_ce, (E_ce.shape[0], 1))
+        w = w - eta * E_ce
 
     test_y = sig(np.dot(X, w))
     test_a = np.dot(np.transpose(C), np.log(test_y.clip(min=min_float)))
@@ -57,7 +58,8 @@ for i in range(1, 1+num_epochs):
     class_rate = np.sum(C == test_y_norm) / float(C.shape[0])
     ce_plot_points.append(test_ce[0, 0])
     mr_plot_points.append(1 - class_rate)
-    # print "error =", test_ce, "class rate =", class_rate
+    if i % 50 == 0:
+        print "epoch =", i, "error =", test_ce, "class rate =", class_rate
 
 # plt.figure()
 # fig, ax = plt.subplots()
