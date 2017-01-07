1466038dfdbe1bd4b1350e73ad102e189f0914d7
Darjan Salaj <salaj@gmx.at>
2017-01-07 23:26:21 +0100

    finding optimal epoch number and learning rate

--- nn16_ex4_load.py
+++ nn16_ex4_load.py
@@ -3,10 +3,9 @@ import pylab as pl     # for graphics
 import numpy as np
 import tensorflow as tf
 
-learning_rate = 0.1
 n_hidden = 20
 batch_size = 40
-n_epochs = 80
+n_epochs = 40
 
 # NOTE:
 # mini-batch size 40
@@ -18,15 +17,16 @@ n_epochs = 80
 # implement early stopping
 # chose learning rate for each algorithm and compare with plots
 
+
 def iterate_minibatches(samples, labels, shuffle=False, batchsize=32):
-    indices = np.arange(samples.shape[0])
-    if shuffle:
-    	np.random.shuffle(indices)
-    for start_idx in range(0, samples.shape[0] - batchsize + 1, batchsize):
-        excerpt = indices[start_idx:start_idx + batchsize]
-        samples_yield = [samples[i] for i in excerpt]
-        labels_yield = [labels[i] for i in excerpt]
-        yield samples_yield, labels_yield
+  indices = np.arange(samples.shape[0])
+  if shuffle:
+    np.random.shuffle(indices)
+  for start_idx in range(0, samples.shape[0] - batchsize + 1, batchsize):
+    excerpt = indices[start_idx:start_idx + batchsize]
+    samples_yield = [samples[i] for i in excerpt]
+    labels_yield = [labels[i] for i in excerpt]
+    yield samples_yield, labels_yield
 
 pl.close('all')   # closes all previous figures
 
@@ -62,7 +62,7 @@ C_onehot[np.arange(n_data), C - np.ones_like(C)] = 1
 C_tst_onehot = np.zeros((n_tst_data, n_classes))
 C_tst_onehot[np.arange(n_tst_data), C_tst - np.ones_like(C_tst)] = 1
 
-# # Create the model
+# # Create the single layer model
 # x = tf.placeholder(tf.float32, [None, n_features])
 # W = tf.Variable(tf.zeros([n_features, n_classes]))
 # b = tf.Variable(tf.zeros([n_classes]))
@@ -71,6 +71,7 @@ C_tst_onehot[np.arange(n_tst_data), C_tst - np.ones_like(C_tst)] = 1
 # y = tf.matmul(x, W) + b
 
 # Create the model with hidden layer
+# TIP: should get about 75% accuracy with two hidden layers
 x = tf.placeholder(tf.float32, [None, n_features])
 W = tf.Variable(tf.truncated_normal([n_features, n_hidden], stddev=0.1))
 b = tf.Variable(tf.constant(0.1, shape=[n_hidden]))
@@ -84,9 +85,14 @@ y = tf.matmul(inpt, Wh) + bh
 # Define loss and optimizer
 y_ = tf.placeholder(tf.float32, [None, n_classes])
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+learning_rate = tf.placeholder(tf.float32)
+
+# optimal learning rate for GD is 0.1
+# train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+# optimal learning rate for Adam is 0.0005
 # train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
-# train_step = tf.train.RMSPropOptimizer(learning_rate).minimize(cross_entropy)
+# optimal learning rate for RMSProp is 0.001
+train_step = tf.train.RMSPropOptimizer(learning_rate).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 tf.initialize_all_variables().run()
@@ -102,24 +108,44 @@ C_train_onehot = C_onehot[:n_train_data]
 X_valid = X[n_train_data:]
 C_valid_onehot = C_onehot[n_train_data:]
 
-max_valid_acc = 0
-opt_epoch_num = 0
-opt_batch_part = 0
-for e_i in range(n_epochs):
-  for b_i, (batch_xs, batch_ys) in enumerate(iterate_minibatches(X_train, C_train_onehot, shuffle=False, batchsize=batch_size)):
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    valid_acc = sess.run(accuracy, feed_dict={x: X_valid, y_: C_valid_onehot})
-    if valid_acc > max_valid_acc:
-      max_valid_acc = valid_acc
-      opt_epoch_num = e_i
-      opt_batch_part = b_i
-  train_acc = sess.run(accuracy, feed_dict={x: X_train, y_: C_train_onehot})
-  test_acc = sess.run(accuracy, feed_dict={x: X_tst, y_: C_tst_onehot})
-  print("test accuracy after", e_i, "epochs =", test_acc,
-        # int(e_i*n_data/batch_size), "batches of size", batch_size, "=",
-        "train accuracy", train_acc,
-        "validation accuracy", valid_acc)
+for lr in [0.0001, 0.0005, 0.0008] + list(np.arange(0.001, 0.01, 0.002)) + \
+          [0.01, 0.02] + list(np.arange(0.05, 0.5, 0.05)):
+  a = W.assign(tf.truncated_normal([n_features, n_hidden], stddev=0.1))
+  sess.run(a, feed_dict={})
+  a = b.assign(tf.constant(0.1, shape=[n_hidden]))
+  sess.run(a, feed_dict={})
+  a = Wh.assign(tf.truncated_normal([n_hidden, n_classes], stddev=0.1))
+  sess.run(a, feed_dict={})
+  a = bh.assign(tf.constant(0.1, shape=[n_classes]))
+  sess.run(a, feed_dict={})
 
-print("Epoch number with best validation accuracy is", opt_epoch_num,
-      ", more precisely", (opt_epoch_num*batch_size)+opt_batch_part,
-      "mini matches of size", batch_size)
+  max_valid_acc = opt_epoch_num = opt_batch_part = train_acc = test_acc = 0
+  train_accs = []
+  test_accs = []
+
+  for e_i in range(n_epochs):
+    for b_i, (batch_xs, batch_ys) in enumerate(iterate_minibatches(X_train, C_train_onehot,
+                                                                   shuffle=False, batchsize=batch_size)):
+      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys,
+                                      learning_rate: lr})
+      valid_acc = sess.run(accuracy, feed_dict={x: X_valid, y_: C_valid_onehot})
+      if valid_acc > max_valid_acc:
+        max_valid_acc = valid_acc
+        opt_epoch_num = e_i
+        opt_batch_part = b_i
+    train_acc = sess.run(accuracy, feed_dict={x: X_train, y_: C_train_onehot})
+    train_accs.append(train_acc)
+    test_acc = sess.run(accuracy, feed_dict={x: X_tst, y_: C_tst_onehot})
+    test_accs.append(test_acc)
+    # print("test accuracy after", e_i, "epochs =", test_acc,
+    #       # int(e_i*n_data/batch_size), "batches of size", batch_size, "=",
+    #       "train accuracy", train_acc,
+    #       "validation accuracy", valid_acc)
+
+  print("Learning rate = %f" % lr,
+        "Epoch number with best validation accuracy is %02d" % opt_epoch_num,
+        "training acc = %.6f" % train_accs[opt_epoch_num],
+        "testing acc = %.6f" % test_accs[opt_epoch_num],
+        # ", more precisely", (opt_epoch_num*batch_size)+opt_batch_part,
+        # "mini matches of size", batch_size
+       )
