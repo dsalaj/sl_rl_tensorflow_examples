63d4e145ea21ca22da1ab59b33258ab49068aac0
Darjan Salaj <salaj@gmx.at>
2017-01-09 20:10:06 +0100

    score based wip

--- a4/pong_non_lin_Q_score_based_exp_rep.py
+++ a4/pong_non_lin_Q_score_based_exp_rep.py
@@ -21,19 +21,26 @@ import tensorflow as tf
 # lr_decay = 1# .9999
 # n_hidden = 60
 
+# Minimal bellman error reached 0.0112
+
 # Algorithm parameters
-learning_rate = .01
-gamma = .999
+learning_rate = .005
+gamma = .99
 epsilon = .5
-epsi_decay = .9999
-lr_decay = .999
+epsilon_min = .1
+epsi_decay = .99
+lr_decay = .9999
 n_hidden = 50
+exploration_period = 100
+exploration_steps = 10000
+exp_pos = 0
+exp_neg = 0
 
 # General parameters
 render = False
 # render = True
 N_print_every = 10
-N_trial = 1000
+N_trial = 10000
 N_trial_test = 100
 trial_duration = 200
 
@@ -42,8 +49,15 @@ env = gym.make("Pong-v0")
 # dim_state = env.observation_space.high.__len__()
 dim_state = 6  # after preprocessing it's 6
 # n_action = env.action_space.n
-n_action = 2
-action_list = [2, 3]  # only 2 controls used (UP and DOWN)
+n_action = 3
+action_list = [0, 2, 3]
+
+
+def neg_prop(mb_rew_sum, i, batch_len):
+    if i > batch_len - 10:  # punish only last 10 frames
+        return (mb_rew_sum * ((i+1) / batch_len)**4,)
+    else:
+        return (0, )
 
 
 def iterate_minibatches(exp_mem, shuffle=False, batchsize=32):
@@ -60,33 +74,29 @@ def iterate_minibatches(exp_mem, shuffle=False, batchsize=32):
         yield exp_mem_yield
 
 
-# Initialize the parameters of the Q model
-wh0 = np.float32(rd.normal(loc=0.0, scale=1./np.sqrt(dim_state), size=(dim_state, n_hidden)))
-bh0 = np.zeros(n_hidden, dtype=np.float32)
-w0 = np.float32(rd.normal(loc=0.0, scale=1./np.sqrt(n_hidden), size=(n_hidden, n_action)))
-b0 = np.zeros(n_action, dtype=np.float32)
-
 # Generate the symbolic variables to hold the state values
 state_holder = tf.placeholder(dtype=tf.float32, shape=(None, dim_state), name='symbolic_state')
 next_state_holder = tf.placeholder(dtype=tf.float32, shape=(None, dim_state), name='symbolic_state')
 
-# Create the parameters of the Q model
-w = tf.Variable(initial_value=w0, trainable=True, name='weight_variable')
-b = tf.Variable(initial_value=b0, trainable=True, name='bias')
-wh = tf.Variable(initial_value=wh0, trainable=True, name='hidden_weight_variable')
-bh = tf.Variable(initial_value=bh0, trainable=True, name='hidden_bias')
+# Create the parameters of the Q model - initializing with non-zero values
+w = tf.Variable(initial_value=tf.truncated_normal([n_hidden, n_action], stddev=0.1), trainable=True, name='weight_variable')
+b = tf.Variable(initial_value=tf.constant(0.1, shape=[n_action]), trainable=True, name='bias')
+wh = tf.Variable(initial_value=tf.truncated_normal([dim_state, n_hidden], stddev=0.1), trainable=True, name='hidden_weight_variable')
+bh = tf.Variable(initial_value=tf.constant(0.1, shape=[n_hidden]), trainable=True, name='hidden_bias')
 
 # Q function at the current step
 a_y = tf.matmul(state_holder, wh, name='output_activation') + bh
 y = tf.nn.relu(a_y, name='hidden_layer_activation')
 a_z = tf.matmul(y, w, name='output_activation') + b
-Q = -tf.nn.softmax(a_z, name='Q_model')
+# Q = -tf.nn.softmax(a_z, name='Q_model')
+Q = a_z
 
 # Q function at the next step
 next_a_y = tf.matmul(next_state_holder, wh, name='next_step_output_activation') + bh
 next_y = tf.nn.relu(next_a_y, name='next_step_hidden_layer_activation')
 next_a_z = tf.matmul(next_y, w, name='next_step_output_activation') + b
-next_Q = -tf.nn.softmax(next_a_z, name='next_step_Q_model')
+# next_Q = -tf.nn.softmax(next_a_z, name='next_step_Q_model')
+next_Q = next_a_z
 
 # Define symbolic variables that will carry information needed for training
 action_holder = tf.placeholder(dtype=tf.float32, shape=(None, n_action, 1), name='symbolic_action')
@@ -102,12 +112,16 @@ gamma_rew = tf.reshape(gamma * tf.reduce_max(next_Q, reduction_indices=1) * (1 -
 next_R = r_holder + gamma_rew
 # variable_summaries(next_R, '/next_R')
 
-error = tf.reduce_mean((R - next_R)**2)
+# error with clipping
+error = tf.clip_by_value(tf.reduce_mean((R - next_R)**2), clip_value_min=-1.0, clip_value_max=1.0)
 variable_summaries(error, '/error')
 
 # Define the operation that performs the optimization
 learning_rate_holder = tf.placeholder(dtype=tf.float32, name='symbolic_state')
-training_step = tf.train.GradientDescentOptimizer(learning_rate_holder).minimize(error)
+# training_step = tf.train.GradientDescentOptimizer(learning_rate_holder).minimize(error)
+# training_step = tf.train.RMSPropOptimizer(learning_rate_holder).minimize(error)
+# RMSProp parameters from BrainDQN
+training_step = tf.train.RMSPropOptimizer(learning_rate_holder, 0.99, 0.0, 1e-6).minimize(error)
 
 sess = tf.Session()  # FOR NOW everything is symbolic, this object has to be called to compute each value of Q
 sess.run(tf.initialize_all_variables())
@@ -153,7 +167,10 @@ for k in range(N_trial + N_trial_test):
     acc_reward = 0  # Init the accumulated reward
     observation = new_observation = env.reset()  # Init the state
     pro_observation = prepro(new_observation, observation)
-    action = policy(pro_observation)  # Init the first action
+    if steps < exploration_steps:
+        action = rd.choice(action_list)
+    else:
+        action = policy(pro_observation)  # Init the first action
 
     trial_err_list = []
 
@@ -169,8 +186,11 @@ for k in range(N_trial + N_trial_test):
 
         exp_mem.append(zip((pro_observation, pro_new_observation, action_list.index(action), reward, done)))
 
+        if k % 9 == 0 and done:
+            print("learning rate: ", learning_rate, "epsilon:", epsilon)
+
         one_hot_action = np.zeros((1, n_action))
-        one_hot_action[0, action_list.index(action)] = 1
+        one_hot_action[0, action_list.index(action)] = 1.0
         # Compute the Bellman Error for monitoring
         err = sess.run(error, feed_dict={
             state_holder: pro_observation.reshape(-1, dim_state),
@@ -183,10 +203,19 @@ for k in range(N_trial + N_trial_test):
 
         observation = new_observation  # Pass the new state to the next step
         pro_observation = pro_new_observation  # Pass the new state to the next step
-        action = policy(pro_observation)  # Decide next action based on policy
+        if steps < exploration_steps:
+            action = rd.choice(action_list)
+        else:
+            action = policy(pro_observation)  # Decide next action based on policy
+
         acc_reward += reward  # Accumulate the reward
         if reward != 0:
-            exp_mem_trials.append(exp_mem)
+            if (reward < 0 and exp_neg <= exp_pos) or (reward > 0 and exp_pos <= exp_neg):
+                exp_mem_trials.append(exp_mem)
+                if reward > 0:
+                    exp_pos += 1
+                else:
+                    exp_neg += 1
             exp_mem = []
         if done:
             break  # Stop the trial when the environment says it is done
@@ -194,13 +223,15 @@ for k in range(N_trial + N_trial_test):
         t += 1
 
     mb_size = 32
-    exp_mem_trials = exp_mem_trials[-50:]
+    exp_mem_trials = exp_mem_trials[-(exploration_period * 10):]
 
-    if len(exp_mem_trials) >= 50 and k % mb_size == 0:
+    # if len(exp_mem_trials) >= exploration_period and k % mb_size == 0:
+    if len(exp_mem_trials) >= exploration_period:
 
         for i, batch in enumerate(iterate_minibatches(exp_mem_trials, shuffle=True, batchsize=1)):
 
-            if i >= mb_size:  # learn for i mini batches
+            # if i >= mb_size:  # learn for i mini batches
+            if i > 0:  # learn only once
                 break
 
             minibatch_zip_ = batch[0]
@@ -210,8 +241,12 @@ for k in range(N_trial + N_trial_test):
 
             mb_rew_sum = sum([r[0] for r in mb_rew])
 
+            # reward propagation for previous frames
             if mb_rew_sum > 0:
-                mb_rew = [(mb_rew_sum,) for i in range(batch_len)]
+                mb_rew = [(mb_rew_sum * ((i+1) / batch_len),) for i in range(batch_len)]
+            if mb_rew_sum < 0:
+                # mb_rew = [(mb_rew_sum * ((i+1) / batch_len)**4,) for i in range(batch_len)]
+                mb_rew = [neg_prop(mb_rew_sum, i, batch_len) for i in range(batch_len)]
 
             mb_obs = np.array(mb_obs)  # [o  for o, no, a, r, d in minibatch_zip])
             mb_nob = np.array(mb_nob)  # [no for o, no, a, r, d in minibatch_zip])
@@ -219,7 +254,7 @@ for k in range(N_trial + N_trial_test):
             mb_rew = np.array(mb_rew)  # [r  for o, no, a, r, d in minibatch_zip])
             mb_don = np.array(mb_don).astype(np.float32)  # [d  for o, no, a, r, d in minibatch_zip])
             one_hot_actions = np.zeros((batch_len, n_action))
-            one_hot_actions[np.arange(batch_len), np.reshape(mb_act, (batch_len,))[:]] = 1
+            one_hot_actions[np.arange(batch_len), np.reshape(mb_act, (batch_len,))[:]] = 1.0
 
             # Perform one step of gradient descent
             summary, _ = sess.run([merged, training_step], feed_dict={
@@ -231,21 +266,25 @@ for k in range(N_trial + N_trial_test):
                 learning_rate_holder: learning_rate})
             train_writer.add_summary(summary, steps)
 
-        if learning_rate > 1e-4:
+        if learning_rate > 0.00025:
             learning_rate *= lr_decay
         else:
-            learning_rate = 1e-4
-        if epsilon > 0.2:
+            learning_rate = 0.00025
+        if epsilon > epsilon_min:
             epsilon *= epsi_decay
         else:
-            epsilon = 0.2
+            epsilon = epsilon_min
+    else:
+        full = len(exp_mem_trials) / exploration_period
+        print("%02d%% of memory ready" % int(full * 100))
 
     # Stack values for monitoring
     err_list.append(np.mean(trial_err_list))
     time_list.append(t + 1)
     reward_list.append(acc_reward)  # Store the result
 
-    print_results(k, time_list, err_list, reward_list)
+    if steps < exploration_steps:
+        print_results(k, time_list, err_list, reward_list, N_print_every=N_print_every)
 
 plot_results(N_trial, N_trial_test, reward_list, time_list, err_list)
 plt.show()
